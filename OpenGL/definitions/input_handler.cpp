11a1f7a3809815dd3b8544ab178e6b9c238ccee8
adityaruplaha <adi24360526@gmail.com>
2018-06-30 19:48:52 +0530

    Input handler upgrade - listen for simulateous pressing

--- OpenGL/definitions/input_handler.cpp
+++ OpenGL/definitions/input_handler.cpp
@@ -17,6 +17,12 @@ void InputHandler::connect(int key, std::function<void()> func)
 	binds.push_back(pair);
 }
 
+void InputHandler::connect(std::vector<int> keys, std::function<void()> func)
+{
+	auto pair = std::make_pair(keys, func);
+	binds_m.push_back(pair);
+}
+
 void InputHandler::disconnect(int key)
 {
 	for (int i = 0; i < binds.size(); i++)
@@ -28,11 +34,35 @@ void InputHandler::disconnect(int key)
 	}
 }
 
+void InputHandler::disconnect(std::vector<int> keys)
+{
+	for (int i = 0; i < binds_m.size(); i++)
+	{
+		if (binds_m[i].first == keys)
+			binds_m.erase(binds_m.begin() + i);
+
+		break;
+	}
+}
+
 void InputHandler::listen()
 {
 	for (auto b : binds)
 	{
 		if (glfwGetKey(attached_window, b.first) == GLFW_PRESS)
 			(b.second)();
+	}
+
+	for (auto b : binds_m)
+	{
+		bool active = true;
+		for (auto k : b.first)
+		{
+			active = active && (glfwGetKey(attached_window, k) == GLFW_PRESS);
+		}
+		if (active)
+		{
+			(b.second)();
+		}
 	}
 }
--- OpenGL/definitions/input_handler.h
+++ OpenGL/definitions/input_handler.h
@@ -10,10 +10,13 @@ public:
 	InputHandler(GLFWwindow* window);
 	void attach(GLFWwindow* window);
 	void connect(int key, std::function<void ()> func);
+	void connect(std::vector<int> keys, std::function<void ()> func);
 	void disconnect(int key);
+	void disconnect(std::vector<int> keys);
 	void listen();
 
 private:
 	GLFWwindow* attached_window;
 	std::vector<std::pair<int, std::function<void()>>> binds;
+	std::vector<std::pair<std::vector<int>, std::function<void()>>> binds_m;
 };
